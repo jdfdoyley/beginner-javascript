a697a2e6c45ea984e37184d8354431411131e6cf
Jason D'Oyley <jdfd.dev@icloud.com>
2020-02-05 06:59:11 -0500

    07 - JavaScript Types: Strings

---
+++ playground/types.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const firstName = 'Jason'
+const middleName = "Augustus"
+const lastName = `D'Oyley`
+
+const sentence = 'Mrs. Garrison\'s dog is such a cute and loyal dog'
+const sentence2 = "Isn't that Martin's bagpack?"
+const sentence3 = `
+One love, one heart
+Lets get together
+And, feel alright
+`
+
+const greeting = `Hello there, my name is ${firstName} ${lastName}. It's a pleasure meeting you!`
