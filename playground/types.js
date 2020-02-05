030266aa1cf9e88202d20b275c6e5a26eb91d1f4
Jason D'Oyley <jdfd.dev@icloud.com>
2020-02-05 07:19:22 -0500

    08 - JavaScript Types: Numbers

--- playground/types.js
+++ playground/types.js
@@ -1,14 +1,29 @@
-/* eslint-disable */
-const firstName = 'Jason'
-const middleName = "Augustus"
-const lastName = `D'Oyley`
+// const firstName = 'Jason'
+// const middleName = "Augustus"
+// const lastName = `D'Oyley`
 
-const sentence = 'Mrs. Garrison\'s dog is such a cute and loyal dog'
-const sentence2 = "Isn't that Martin's bagpack?"
-const sentence3 = `
-One love, one heart
-Lets get together
-And, feel alright
-`
+// const sentence = 'Mrs. Garrison\'s dog is such a cute and loyal dog'
+// const sentence2 = "Isn't that Martin's bagpack?"
+// const sentence3 = `
+// One love, one heart
+// Lets get together
+// And, feel alright
+// `
 
-const greeting = `Hello there, my name is ${firstName} ${lastName}. It's a pleasure meeting you!`
+// const greeting = `Hello there, my name is ${firstName} ${lastName}. It's a pleasure meeting you!`
+
+// const age = 30;
+// const fullName = "Jason D'Oyley";
+
+// const a = 10;
+// const b = 20;
+
+const skittles = 43;
+const friends = 3;
+
+const eachFriendGets = Math.floor(skittles / friends);
+const leftOvers = skittles % friends;
+
+console.log(
+  `Each friend gets ${eachFriendGets} skittles. And, ${leftOvers} left over for me.`
+);
