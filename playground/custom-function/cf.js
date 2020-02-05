5400797820c8a766d4190782c0aa56467c23a7f6
Jason D'Oyley <jdfd.dev@icloud.com>
2020-02-05 16:43:14 -0500

    14 - JavaScript Function: Parameters & Arguments

--- playground/custom-function/cf.js
+++ playground/custom-function/cf.js
@@ -1,14 +1,32 @@
-// Sales Tax = 6%
-const SALES_TAX = 0.06;
-
 // Function Definition
-function calculateBill(amount) {
-  // Function Body
-  // return console.log('Running Calculate Bill!');
+// function calculateBill(billAmount, taxRate = 0.06) {
+// Function Body
+// return console.log('Running Calculate Bill!');
 
-  return amount * (1 + SALES_TAX);
-}
+//   return billAmount * (1 + taxRate);
+// }
 
 // Function Call
-const billTotal = calculateBill(100);
-console.log(`Bill Total $${billTotal.toFixed(2)}`);
+// const billTotal = calculateBill(100, 0.13);
+// console.log(`Bill Total $${billTotal.toFixed(2)}`);
+
+// function doctorize(name) {
+//   return `Dr. ${name}`;
+// }
+
+// function yell(name) {
+//   return `HEY, ${name.toUpperCase()}!!!`;
+// }
+
+// const doctorize = name => `Dr. ${name}`;
+// const yell = name => `HEY, ${name.toUpperCase()}!!!`;
+
+// const alert = yell(doctorize('Mark'));
+// console.log(alert);
+
+function calculateBill(billAmount, taxRate = 0.13, tip = 0.15) {
+  return billAmount * (1 + taxRate + tip);
+}
+
+const total = calculateBill(86, undefined, 0.2);
+console.log(`Your total is $${total.toFixed(2)}.`);
