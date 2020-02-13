d55657c8e12c56a089a065148ffebf4a36c02fc8
Jason D'Oyley <jdfd.dev@icloud.com>
2020-02-13 06:11:26 -0500

    24 DOM: Built in and Custom Data

--- dom/dom.js
+++ dom/dom.js
@@ -6,3 +6,11 @@ function roundClass() {
 }
 
 pic.addEventListener('click', roundClass);
+
+// Attribute
+pic.alt = 'random pictures'; // setters
+console.log(pic.alt); // getters
+
+// getters and setters
+pic.setAttribute('alt', 'great random pics');
+console.log(pic.getAttribute('alt'));
