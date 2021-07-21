2d23bdabb6f47e019b1d61dffec673b07e3fd849
Jason D'Oyley <jdfd.dev@icloud.com>
2021-07-21 10:27:04 -0400

    intro to js types

---
+++ playground/types.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+const name = "Jason";
+const middle = "Diego";
+const last = "D'Oyley";
+const lastTwo = "D'Oyley";
+const sentence = 'She\'s so "cool"';
+const sentence2 = `She's so "cool"`;
+
+console.log(name);
+console.log(middle);
+console.log(last);
+console.log(lastTwo);
+console.log(sentence);
